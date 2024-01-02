35887d9126183486e420415ba0c2bb0b18249790
theophiluskibet <kibettheophilus@gmail.com>
2024-01-02 12:07:11 +0300

    Initial commit

---
+++ src/main/java/com/theophiluskibet/kenyancounties/KenyanCountiesApplication.java
@@ -0,0 +1,13 @@
+package com.theophiluskibet.kenyancounties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenyanCountiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KenyanCountiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theophiluskibet/kenyancounties/KenyanCountiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theophiluskibet.kenyancounties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenyanCountiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
